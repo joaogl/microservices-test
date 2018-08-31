a0bd10028f8c11f87b2717a922debbdcf4181a74
Joao Lourenco <jlourenco@voxbone.com>
2018-08-31 19:13:44 +0200

    Getting the base projects up.

---
+++ comments-service/src/main/java/net/joaolourenco/blog/commentsservice/CommentsServiceApplication.java
@@ -0,0 +1,12 @@
+package net.joaolourenco.blog.commentsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsServiceApplication.class, args);
+	}
+}
---
+++ mailing-service/src/main/java/net/joaolourenco/blog/mailingservice/MailingServiceApplication.java
@@ -0,0 +1,12 @@
+package net.joaolourenco.blog.mailingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailingServiceApplication.class, args);
+	}
+}
