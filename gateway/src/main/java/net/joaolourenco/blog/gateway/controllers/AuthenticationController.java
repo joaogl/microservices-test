c1b2f56761c247308bc97ec407308afa1fcfa000
João Lourenço <joaoguerralourenco.pt@gmail.com>
2018-09-17 23:36:53 +0200

    Cleaning up the code.

--- gateway/src/main/java/net/joaolourenco/blog/gateway/controllers/AuthenticationController.java
+++ gateway/src/main/java/net/joaolourenco/blog/gateway/controllers/AuthenticationController.java
@@ -3,8 +3,6 @@ package net.joaolourenco.blog.gateway.controllers;
 import net.joaolourenco.blog.common.AvailableServices;
 import net.joaolourenco.blog.common.BaseController;
 import net.joaolourenco.blog.common.domain.authentication.UserCredentialsDTO;
-import net.joaolourenco.blog.common.exceptions.errors.LoginErrorContants;
-import net.joaolourenco.blog.common.exceptions.exceptions.LoginException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClientException;
 
 @RestController
 @RequestMapping("auth")
@@ -24,21 +21,22 @@ public class AuthenticationController extends BaseController {
     ResponseEntity<Boolean> login(@RequestBody() final UserCredentialsDTO user) {
         LOGGER.info("Attempting to login user: " + user.getUsername());
 
-        try {
-            return ResponseEntity.ok(restTemplate.postForObject(getServiceURL(AvailableServices.AuthenticationService, "auth/login"), user, Boolean.class));
-        } catch (RestClientException e) {
-            throw e;
+        String urlEndpoint = getServiceURL(AvailableServices.AuthenticationService, "auth/login");
 
-            // throw new LoginException(LoginErrorContants.ERR_LOGIN_INVALID);
-        }
+        Boolean loginResponse = restTemplate.postForObject(urlEndpoint, user, Boolean.class);
 
+        return ResponseEntity.ok(loginResponse);
     }
 
     @PostMapping("/register")
     ResponseEntity<Long> register(@RequestBody() final UserCredentialsDTO user) {
         LOGGER.info("Attempting to register new user user: " + user.getUsername());
 
-        return ResponseEntity.ok(restTemplate.postForObject(getServiceURL(AvailableServices.AuthenticationService, "auth/register"), user, Long.class));
+        String urlEndpoint = getServiceURL(AvailableServices.AuthenticationService, "auth/register");
+
+        Long userId = restTemplate.postForObject(urlEndpoint, user, Long.class);
+
+        return ResponseEntity.ok(userId);
     }
 
 }
